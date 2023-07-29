12f0dbd50f82513803409560ae4460c88ac11ffb
Alan Anson <alan.anson@live.fr>
2023-07-29 09:07:06 +0200

    In one go

--- Music_dl.py
+++
@@ -1,5 +0,0 @@
-import moviepy.editor as mp
-source = input('Entrer la video à mettre en music : ')
-clip = mp.VideoFileClip(source)
-destination = input("l'emplacement du fichier : ")
-clip.audio.write_audiofile(destination)
